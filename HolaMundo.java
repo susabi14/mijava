4859143e4f2c466e27248fd53ee83fe2cad0445c
Susan Lacayo <susan.lacayo@std.uni.edu.ni>
2025-06-25 17:47:30 -0600

    cambio

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,12 +1,14 @@
 public class HolaMundo{
     public static void main(String[]args){
-        String nombre = "Estudiante";
+        String nombre = "Susan Lacayo";
+        String carnet =  "20252763U";
 
         int a = 10;
         int b = 20;
         int suma= a + b;
 
-        System.out.println("Hola "+ nombre);
+        System.out.println("Hola soy"+ nombre);
+        System.out.println(carnet);
         System.out.println("La suma de "+ a + " + " + b + " es: " + suma);
     }
 }
